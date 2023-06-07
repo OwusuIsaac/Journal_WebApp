f12786fde4ad0b602a646fa726a509e9350b78ac
OwusuIsaac <105105471+OwusuIsaac@users.noreply.github.com>
2023-06-07 23:53:25 +0200

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+// import styles from "../styles/footer.module.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-padding"></div>
+      <div className="footer">
+        <p className="author"> Desmond Ofori Atta</p>
+        <p className="copyright text-muted">&copy; 2021</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
